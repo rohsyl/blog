565796ccf65953dfab51f9f2b6770d85f145c81a
rohs <sylvain@rohs.ch>
2025-11-26 20:51:46 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Html from "@/app/components/Html";
 import Header from "@/app/components/Header";
+import Head from "next/head";
 
 
 export const metadata = {
@@ -17,6 +18,11 @@ export default function RootLayout({
 
   return (
       <Html>
+          <Head>
+              {process.env.NODE_ENV == "production" && (
+                  <script defer src="https://analytics.rohs.ch/script.js" data-website-id="b3deab3d-77e1-4a19-a8c6-8d02d71d72b8"></script>
+              )}
+          </Head>
           <Header />
           <div className="font-mono md:pt-30">
             {children}
